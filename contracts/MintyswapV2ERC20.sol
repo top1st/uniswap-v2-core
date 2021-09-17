e92a534ee05f9211403b6491923554bff32044b8
top1st <riguang102@gmail.com>
2021-09-17 21:53:53 +0800

    Update MintyswapV2ERC20.sol

--- contracts/MintyswapV2ERC20.sol
+++ contracts/MintyswapV2ERC20.sol
@@ -6,8 +6,8 @@ import './libraries/SafeMath.sol';
 contract MintyswapV2ERC20 is IMintyswapV2ERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'Uniswap V2';
-    string public constant symbol = 'UNI-V2';
+    string public constant name = 'Mintyswap V2';
+    string public constant symbol = 'Minty-V2';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
