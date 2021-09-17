0e7c7ebcb01fe6c314cf7f14ed07b1f78a2d9b87
top1st <riguang102@gmail.com>
2021-09-17 21:54:54 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Factory = await hre.ethers.getContractFactory("MintyswapV2Factory");
+  const factory = await Factory.deploy();
 
-  await greeter.deployed();
+  await factory.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("factory deployed to:", factory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
